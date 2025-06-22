e3d3b826349486cabb034d0c79bf707bb46389a3
Saber <81108166+Saber422@users.noreply.github.com>
2025-06-22 16:20:54 +0800

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -397,15 +397,15 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
   # - angle offset: most steering angle sensors are offset and measure a non zero angle when driving straight
   # This alert is thrown when any of these values exceed a sanity check. This can be caused by
   # bad alignment or bad sensor data. If this happens consistently consider creating an issue on GitHub
-  EventName.vehicleModelInvalid: {
-    ET.NO_ENTRY: NoEntryAlert("Vehicle Parameter Identification Failed"),
-    ET.SOFT_DISABLE: SoftDisableAlert("Vehicle Parameter Identification Failed"),
-    ET.WARNING: Alert(
-      "Vehicle Parameter Identification Failed",
-      "",
-      AlertStatus.normal, AlertSize.small,
-      Priority.LOWEST, VisualAlert.steerRequired, AudibleAlert.none, .0, .0, .1),
-  },
+  #EventName.vehicleModelInvalid: {
+  #  ET.NO_ENTRY: NoEntryAlert("Vehicle Parameter Identification Failed"),
+  #  ET.SOFT_DISABLE: SoftDisableAlert("Vehicle Parameter Identification Failed"),
+  #  ET.WARNING: Alert(
+  #    "Vehicle Parameter Identification Failed",
+  #    "",
+  #    AlertStatus.normal, AlertSize.small,
+  #    Priority.LOWEST, VisualAlert.steerRequired, AudibleAlert.none, .0, .0, .1),
+  #},
 
   EventName.steerTempUnavailableSilent: {
     ET.WARNING: Alert(
